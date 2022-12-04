ea57cb4474404b5aa77ba61c8e6247f3f49cbee1
raypoe <daedalus610@hotmail.com>
2022-12-04 00:44:13 -0300

    rompiendome la cabeza jaja

---
+++ javascript/main.js
@@ -0,0 +1,36 @@
+let edad="18";
+
+function ingreso(){
+
+    let ingresar=false;
+    let edadUsuario=prompt("Ingresa tu edad en números. Debés ser mayor de edad para utilizar nuestros servicios")
+    if (edadUsuario>=edad){
+        alert ("podés calcular tu préstamo.")
+        ingresar=true
+        
+    }else{
+        alert("Error. Debés ser mayor de edad.")
+    }
+    return ingresar;
+
+
+}
+//Accedemos a simular el prestamo
+let exito=ingreso(true);
+
+if(exito){
+     
+    let monto=prompt("Escribe el monto solicitado")
+    let cuotas=prompt("Escribe la cantidad de cuotas mensuales")
+    let interes=1.10
+    
+    let resultado=(monto / cuotas) * interes
+    let final=alert("Pagarías una suma mensual de $" + resultado)
+    
+
+
+}
+
+
+
+console.log(ingreso);
