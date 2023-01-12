86ba33e6a6bd541a5941931c7d0a7261d2148a28
raypoe <daedalus610@hotmail.com>
2023-01-12 18:22:28 -0300

    mejorando proyecto

---
+++ javascript/productos.js
@@ -0,0 +1,17 @@
+const productos = [
+    {
+        id: 1,
+        nombre: "Pedigree perros x 21kg",
+        precio: 8400,
+        img: "https://puppis.vteximg.com.br/arquivos/ids/182149-160-160/135038.jpg?v=637650377480400000",
+        cantidad: 1,
+    },
+    {
+        id: 2,
+        nombre:"Excellent gatos x 7,5kg",
+        precio: 8600,
+        img: "https://puppis.vteximg.com.br/arquivos/ids/184560-160-160/150065.png?v=637750189267430000",
+        cantidad: 1,
+    },
+
+];
